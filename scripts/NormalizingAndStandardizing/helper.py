f824516b15d94434cc97bcbbf13bcdfca633596b
Nihal Ramaswamy <41371273+nihal-ramaswamy@users.noreply.github.com>
2020-10-25 19:34:10 +0530

    Updated file

--- scripts/NormalizingAndStandardizing/helper.py
+++ scripts/NormalizingAndStandardizing/helper.py
@@ -4,28 +4,31 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 class NormStd:
-    def __init__(self, dfColumn):
-        self.array = np.array(dfColumn)
+    __array = []
 
-    def getZscore(self, array):
+    def __init__(self, dfColumn):
+        self.__array = np.array(dfColumn)
+
+    def __obtain_the_Z_score_value(self, array):
         return scipy.stats.zscore(array)
 
-    def getPvalue(self, array):
+    def __obtain_the_P_value(self, array):
         return scipy.stats.norm.sf(abs(array)) * 2
 
-    def normalize(self, array):
-        return self.array / np.linalg.norm(self.array)
+    def __normalize_the_array_using_this_function(self, array):
+        return array / np.linalg.norm(array)
 
-    def getValues(self, label, mainDictionary=dict()):
-        values = {'mean': self.array.mean(), 'variance': self.array.var(
-        ), 'normalized': self.normalize(self.array), 'z-score': self.getZscore(self.array)}
+    def NormalizeAndStandardize(self, label, mainDictionary=dict()):
+        values = {'mean': self.__array.mean(), 'variance': self.__array.var(
+        ), 'normalized': self.__normalize_the_array_using_this_function(self.__array), 'z-score': self.__obtain_the_Z_score_value(self.__array)}
         normArr = values['normalized']
         values['normMean'] = round(normArr.mean(), 3)
         values['normVar'] = round(normArr.var(), 3)
-        values['pValue']: self.getPvalue(np.array(values['z-score']))
+        values['pValue']: self.__obtain_the_P_value(
+            np.array(values['z-score']))
         zscoreArr = values['z-score']
         values['stdMean'] = round(zscoreArr.mean(), 3)
         values['stdVar'] = round(zscoreArr.var(), 3)
-        values['inputArray'] = self.array
+        values['inputArray'] = self.__array
         mainDictionary[label] = values
         return mainDictionary
