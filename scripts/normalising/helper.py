4c342826b67dc68c2b06e65c9b6ee7592363fecf
Nihal Ramaswamy <41371273+nihal-ramaswamy@users.noreply.github.com>
2020-10-25 19:15:42 +0530

    Update helper.py

--- scripts/normalising/helper.py
+++ scripts/normalising/helper.py
@@ -1,31 +1,34 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import scipy
+from sklearn.preprocessing import MinMaxScaler
 
 
-def getZscore(array):
-    return (array - array.mean(axis=0)) / array.std(axis=0)
+class NormStd:
+    __array = []
 
+    def __init__(self, dfColumn):
+        self.__array = np.array(dfColumn)
 
-def getPvalue(array):
-    return scipy.stats.norm.sf(abs(array)) * 2
+    def __obtain_the_Z_score_value(self, array):
+        return scipy.stats.zscore(array)
 
+    def __obtain_the_P_value(self, array):
+        return scipy.stats.norm.sf(abs(array)) * 2
 
-def normalize(array):
-    newArray = (array - array.min()) / (array.max() - array.min())
-    return newArray
+    def __normalize_the_array_using_this_function(self, array):
+        return array / np.linalg.norm(array)
 
-
-def getValues(array):
-    values = {'mean': array.mean(), 'variance': array.var(
-    ), 'normalized': normalize(array), 'z-score': getZscore(array)}
-    normArr = values['normalized']
-    values['normMean'] = round(normArr.mean(), 3)
-    values['normVar'] = round(normArr.var(), 3)
-    values['pValue']: getPvalue(values['z-score'])
-    zscoreArr = values['z-score']
-    values['stdMean'] = round(zscoreArr.mean(), 3)
-    values['stdVar'] = round(zscoreArr.var(), 3)
-    return values
+    def NormalizeAndStandardize(self, label, mainDictionary=dict()):
+        values = {'mean': self.__array.mean(), 'variance': self.__array.var(
+        ), 'normalized': self.__normalize_the_array_using_this_function(self.__array), 'z-score': self.__obtain_the_Z_score_value(self.__array)}
+        normArr = values['normalized']
+        values['normMean'] = round(normArr.mean(), 3)
+        values['normVar'] = round(normArr.var(), 3)
+        values['pValue']: self.__obtain_the_P_value(
+            np.array(values['z-score']))
+        zscoreArr = values['z-score']
+        values['stdMean'] = round(zscoreArr.mean(), 3)
+        values['stdVar'] = round(zscoreArr.var(), 3)
+        values['inputArray'] = self.__array
+        mainDictionary[label] = values
+        return mainDictionary
